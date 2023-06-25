0975075a6f1cf2b2d620e9fd3ac77a61ed148295
Colton Brenneman <colton.brenneman@gmail.com>
2023-06-25 13:20:49 -0600

    finished storyboard with proper subclassing

---
+++ Assessment5IOS5/Assessment5IOS5/Controllers/ViewControllers/ViewControllers/AlbumDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AlbumDetailViewController.swift
+//  Assessment5IOS5
+//
+//  Created by Colton Brenneman on 6/25/23.
+//
+
+import UIKit
+
+class AlbumDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
